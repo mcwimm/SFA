f34bdf02c798d9fd9dc22c945e0a26dcec37d27c
mcwimm <48250781+mcwimm@users.noreply.github.com>
2020-05-28 19:22:44 +0200

    Update

--- ui.R
+++ ui.R
@@ -1,3 +1,22 @@
+##############
+## PACKAGES ##
+##############
+
+library("shiny")
+library("shinydashboard")
+library("shinycssloaders")  # enables loading signs
+library("markdown")  # enables markdown format
+library("DT")
+
+
+# library("ggforce")  # geom_circle
+# library("ggpubr")  # ggarrange
+library("tidyverse")
+# library("document")
+# library("scales")  # scientif number format
+
+
+
 source("set-up.R")
 
 
