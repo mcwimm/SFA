ab21733386daaeaec49679fafe719ce49ea02cda
marie <mcwimm@bitbucket.org>
2020-06-06 21:10:58 +0200

    [www] update project setting appearance

--- www/project-settings.js
+++ www/project-settings.js
@@ -3,16 +3,26 @@ var setProjectButton = document.getElementById('crtPrj'),
 	nav = document.querySelector('.navbar-static-top');
 
 projectContainer.style.cssText = 'float: right; height: 50px; line-height: 50px; margin-right: 30px;';
-projectContainer.textContent = 'Project: -';
+projectContainer.innerHTML = 'Project: <span style="color:#f00">No project chosen</span>';
 
 nav.appendChild(projectContainer);
 
-function showProjectInHeader() {
-	var prjName = document.getElementById('prjName').textContent;
 
+function showProjectInHeader(prjName) {
 	projectContainer.textContent = 'Project: ' + prjName;
 }
 
-window.addEventListener('load', function(){ 
-	setProjectButton.addEventListener('click', showProjectInHeader);
+
+window.addEventListener('load',function(){
+	var targetNode = document.getElementById('prjName'),
+		config = { childList: true },
+		observer = new MutationObserver(function(mutationsList, observer) {
+		    for(var mutation of mutationsList) {
+		        if (mutation.type == 'childList') {
+		            showProjectInHeader(targetNode.textContent)
+		        }
+		    }
+		});
+
+	observer.observe(targetNode, config);
 });
