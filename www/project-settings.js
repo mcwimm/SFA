308f766f464140c1b4b9b98fa77c59cd52b0dd0a
marie <mcwimm@bitbucket.org>
2020-06-24 21:32:51 +0200

    [style] update

--- www/project-settings.js
+++ www/project-settings.js
@@ -1,9 +1,10 @@
 var setProjectButton = document.getElementById('crtPrj'),
+	setNameButton = document.getElementById('setData'),
 	projectContainer = document.createElement('span'),
 	nav = document.querySelector('.navbar-static-top');
 
 projectContainer.style.cssText = 'float: right; height: 50px; line-height: 50px; margin-right: 30px;';
-projectContainer.innerHTML = 'Project: <span style="color:#f00">No project chosen</span>';
+projectContainer.innerHTML = 'Project: <span style="color:#f00">No project chosen </span>';
 
 nav.appendChild(projectContainer);
 
@@ -12,8 +13,12 @@ function showProjectInHeader(prjName) {
 	projectContainer.textContent = 'Project: ' + prjName;
 }
 
+function showFileNameInHeader(fileName) {
+	projectContainer.textContent = projectContainer.textContent + ' | File : ' + fileName;
+}
 
-window.addEventListener('load',function(){
+
+window.addEventListener('load', function(){
 	var targetNode = document.getElementById('prjName'),
 		config = { childList: true },
 		observer = new MutationObserver(function(mutationsList, observer) {
@@ -25,4 +30,11 @@ window.addEventListener('load',function(){
 		});
 
 	observer.observe(targetNode, config);
+
+	setNameButton.addEventListener('click', function(){
+		fileName = document.querySelector('[for="file1"] + .input-group > input').value;
+		showFileNameInHeader(fileName);
+	})
 });
+
+
