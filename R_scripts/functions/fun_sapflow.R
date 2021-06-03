7f7d8f9fc88d5041746fea8de0168f86a7cec591
marie <mcwimm@bitbucket.org>
2021-06-03 17:50:38 +0200

    [sf] correct functions to calculate sap flow rates

--- R_scripts/functions/fun_sapflow.R
+++ R_scripts/functions/fun_sapflow.R
@@ -13,59 +13,11 @@ get.sapFlowDensity <- function(method = "HFD", data, Dst, Zax, Ztg,
 
 #### Scale tree-level ####
 
-# treeScaleSimple1 = function(data.depth, sensor.dist){
-#    depth = abs(data.depth$depth[1])
-#    data.depth$Asd = pi * (depth^2 - (depth - sensor.dist)^2)
-#    data.depth$sfdepth = data.depth$SFDsw * data.depth$Asd
-#    return(data.depth)
-# }
-
-
-# treeScaleSimple2 = function(data, swd){
-#    if (max(abs(data$depth[1]) <= swd)){
-#       Asw = pi * max(abs(data$depth[1]))^2
-#    } else {
-#       Asw = pi * (max(abs(data$depth[1]))^2 - swd^2)
-#    }
-#
-#    data = data %>%
-#       group_by(datetime) %>%
-#       distinct(menaSfs = mean(SFS), doy, dTime) %>%
-#       mutate(meanSFD = menaSfs /swd) %>%
-#       mutate(sf = meanSFD *Asw)
-#
-#    return(data)
-# }
-#
-# treeScaleSimple3 = function(data.depth){
-#    depth = abs(data.depth$depth[1]) -0.5
-#
-#    data.depth$Csd = 2 * pi * depth
-#    data.depth$sfdepth = data.depth$SFS * data.depth$Csd
-#    return(data.depth)
-# }
-
-
-treeScaleSimple1 <- function(data, depths, swd) {
-   print("In method treeScaleSimple1")
-   
-   depths = sort(depths, decreasing = T)
-   print(depths)
-   Aring = do.call(rbind,
-                   lapply(2:length(depths), function(x)
-                      data.frame(x = depths[x - 1], 
-                                 Aring = pi * abs(depths[x] ^ 2 - (depths[x - 1]) ^ 2))))
-   Aring = rbind(Aring, data.frame(
-      x = tail(depths, 1),
-      Aring = pi * abs(tail(depths, 1) ^ 2 - swd ^
-                          2)
-   ))
-   print(Aring)
-   data = merge(data, Aring, by.x = "depth", by.y = "x")
-   
-   
+treeScaleSimple1 <- function(data, swd) {
+   # Calculate sap flow rate at each sensor depth
    data$SFdepth = data$SFDsw * data$Aring
    
+   # Calculate sap flow rate per time step over alls depths in kg/h
    data = data %>%
       group_by(datetime) %>%
       mutate(sfM1 = sum(SFdepth) / 1000) %>%
@@ -73,106 +25,50 @@ treeScaleSimple1 <- function(data, depths, swd) {
    return(data)
 }
 
-treeScaleSimple2 <- function(data, depths, swd) {
-   print("In method treeScaleSimple2")
+treeScaleSimple2 <- function(data, swd) {
+   depths = unique(data$depth)
+   
+   # Calculate mean sap flow per section and divide it by sap wood depth
    data = data %>%
       group_by(datetime) %>%
-      mutate(SFS_mean = mean(SFS), SFD_mean = mean(SFS) / swd) %>%
+      mutate(SFS_mean = mean(SFS, na.rm = T),
+             SFD_mean = mean(SFS_mean, na.rm = T) / swd) %>% 
       ungroup()
    
-   data$SWDarea = pi * (max(depths) ^ 2 - swd ^ 2)
+   # Calculate sap wood area, which is the difference in total stem area (A_rxy) and
+   # heart wood area
+   A_rxy = pi * max(depths)^2
+   A_hw = pi * (max(depths) - swd)^2
+   data$SWDarea = A_rxy - A_hw
+
+   # Calculate sap flow rate per time step over alls depths in kg/h
    data$sfM2 = data$SFD_mean * data$SWDarea / 1000
    return(data)
 }
 
-treeScaleSimple3 <- function(data, depths, swd) {
-   print("In method treeScaleSimple3")
-   depths = sort(depths, decreasing = T)
-   print(depths)
-   Cring = do.call(rbind,
-                   lapply(2:length(depths), function(x)
-                      data.frame(x = depths[x - 1], Cring = pi * abs(depths[x] + (depths[x -
-                                                                                            1])))))
-   Cring = rbind(Cring, data.frame(x = tail(depths, 1),
-                                   Cring = pi * abs(tail(depths, 1) + swd)))
-   print(Cring)
-   
-   
-   data = merge(data, Cring, by.x = "depth", by.y = "x")
+treeScaleSimple3 <- function(data, swd) {
+   data$SFdepthm3 = data$SFS * data$Cring
    
    data = data %>%
-      mutate(SFdepthm3 = SFS * Cring) %>%
       group_by(datetime) %>%
-      mutate(sfM3 = mean(SFdepthm3) / 1000) %>%
+      mutate(sfM3 = mean(SFdepthm3, na.rm = T) / 1000) %>%
       ungroup()
    return(data)
 }
 
 get.sapFlowByMethod <- function(data, method,
                                 swd) {
-   # ,
-   # sensor.dist){
-   
-   print("Method in get.sapFlowByMethod")
-   print(method)
-   
-   # data = merge(data, depths, by = "position")
-   depths = unique(data$depth)
-   
    if (method == "treeScaleSimple1") {
-      print("1")
-      
-      return(treeScaleSimple1(data, depths, swd))
+      return(treeScaleSimple1(data, swd))
    }
    
    if (method == "treeScaleSimple2") {
-      print("2")
-      return(treeScaleSimple2(data, depths, swd))
-      
+      return(treeScaleSimple2(data, swd))
    }
    
    if (method == "treeScaleSimple3") {
-      print("3")
-      
-      return(treeScaleSimple3(data, depths, swd))
-      
+      return(treeScaleSimple3(data, swd))
    }
-   
-   # if (method == "treeScaleSimple1"){
-   #    print("In method 1")
-   #    d = do.call(rbind,
-   #                lapply(depths, function(x)
-   #                   treeScaleSimple1(data.depth = data[data$depth == x, ],
-   #                                    sensor.dist = sensor.dist)
-   #                ))
-   #    print(head(d))
-   #    newData = d %>%
-   #       group_by(datetime) %>%
-   #       distinct(sf = sum(sfdepth) / 1000, doy = doy, dTime = dTime)
-   # }
-   #
-   # if (method == "treeScaleSimple2"){
-   #    newData = treeScaleSimple2(data, swd) %>%
-   #       distinct(sf = sf / 1000, doy = doy, dTime = dTime)
-   # }
-   #
-   # if (method == "treeScaleSimple3"){
-   #
-   #    d = do.call(rbind,
-   #                lapply(depths, function(x)
-   #                   treeScaleSimple3(data.depth = data[data$depth == x, ])
-   #                ))
-   #    newData = d %>%
-   #       group_by(datetime) %>%
-   #       distinct(sf = mean(sfdepth) / 1000, doy = doy, dTime = dTime) %>%
-   #       ungroup()
-   # }
-   # return(newData)
 }
 
 
-# d1=get.sapFlowByMethod(data = data,
-#                     method = "treeScaleSimple1",#
-#                     swd = 5,
-#                     depths = spruce.depths
-#                   )
