d720682f9a3f8d6de8fbdcedae4373fe8e631437
marie <mcwimm@bitbucket.org>
2022-09-21 12:49:14 +0200

    update footer

--- ui.R
+++ ui.R
@@ -9,9 +9,12 @@ shinyUI(
         dashboardSidebar(
             br(), br(), br(),
             sidebarMenu(menuOutput()),
-            br(), br(), br(),
-            br(), br(), br(), tags$hr(),
-            tags$footer("By Marie-Christin Wimmler", align = "center"),
+            br(), br(), #br(),
+            # br(), br(), br(),
+            tags$hr(),
+            tags$footer("Chair of Forest Biometrics", align = "center"),
+            tags$footer("and", align = "center"),
+            tags$footer("Systems Analysis", align = "center"),
             tags$footer("2020 - 2022", align = "center"),
             tags$hr()
             
